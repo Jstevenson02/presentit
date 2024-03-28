01c0834ca7a581153618aca44b1e21c476e89d7d
jstevenson02 <jacobryanstevenson@gmail.com>
2024-03-28 14:47:43 -0400

    fixing

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -48,27 +48,9 @@ const TransformationForm = ({
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
-        <FormField
-          control={form.control}
-          name='username'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder='shadcn' {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type='submit'>Submit</Button>
-      </form>
-    </Form>
+    <>
+      <div>Form</div>
+    </>
   );
 };
 
